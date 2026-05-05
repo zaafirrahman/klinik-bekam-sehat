72f2604a3125d62736e392613c42b0ee83d42029
zaafirrahman <zaafir123rahman@gmail.com>
2026-05-05 23:40:38 +0700

    fix: maskable icon

--- vite.config.js
+++ vite.config.js
@@ -22,12 +22,14 @@ export default defineConfig({
           {
             src: '/images/icon-192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: '/images/icon-512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
